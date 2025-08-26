d3f22a4cb766f4cc0045614f86bf3a3ff18d6d37
Kapil <kapilgangwar2003@gmail.com>
2025-08-27 04:56:47 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
   </React.StrictMode>,
 )
